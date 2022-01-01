684f8404bc669cb7121250d248269671f30df679
easlie114514 <Y1006604677@163.com>
2022-01-01 11:22:10 +0800

    Initial commit

---
+++ utlis_0.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+def initializeTrackbars(intialTracbarVals=0):
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.createTrackbar("Threshold1", "Trackbars", 200, 255, nothing)
+    cv2.createTrackbar("Threshold2", "Trackbars", 200, 255, nothing)
+
+def valTrackbars():
+    Threshold1 = cv2.getTrackbarPos("Threshold1", "Trackbars")
+    Threshold2 = cv2.getTrackbarPos("Threshold2", "Trackbars")
+    src = Threshold1, Threshold2
+    return src
