dfd989364143ca96851ef66275e997f90c399807
Chandman <619179055@qq.com>
2022-01-03 09:55:22 +0800

    Final

--- ScannerMain.py
+++ ScannerMain.py
@@ -77,3 +77,13 @@ while True:
 
     stackedImage = utlis_0.stackImages(imageArray, 0.75, lables)
     cv2.imshow("Result", stackedImage)
+    # 按s保存
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("Scanned/myImage"+str(count)+".jpg", imgWarpColored)
+        cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
+                      (1100, 350), (0, 255, 0), cv2.FILLED)
+        cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
+                    cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+        cv2.imshow('Result', stackedImage)
+        cv2.waitKey(300)
+        count += 1
